ca3d03c3f2c39760dbdf44cde4ca18211f7f3154
lechau <lehuyenchau12@gmail.com>
2024-10-19 21:14:46 +0700

    [update] - test

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -142,11 +142,10 @@ def _load_cache():
     cache = msal.SerializableTokenCache()  # Create a new MSAL cache
     if session.get("token_cache"):
         cache.deserialize(session["token_cache"])  # Load the cache from the session
-    return cache
+    return None
 
 def _save_cache(cache):
-    if cache.has_state_changed():
-        session["token_cache"] = cache.serialize()  # Save the cache to the session
+    pass
 
 def _build_msal_app(cache=None, authority=None):
     return msal.ConfidentialClientApplication(
